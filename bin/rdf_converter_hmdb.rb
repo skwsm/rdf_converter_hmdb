cc069ea3fc9d7e2a79764465505d0506fa27918f
Shuichi Kawashima <shuichi.kawashima@gmail.com>
2022-09-08 17:11:40 +0900

    fix some bugs

--- bin/rdf_converter_hmdb.rb
+++ bin/rdf_converter_hmdb.rb
@@ -20,6 +20,7 @@ module HMDB
      "up: <http://purl.uniprot.org/uniprot/>",
      "xsd: <http://www.w3.org/2001/XMLSchema#>",
      "pubmed: <http://rdf.ncbi.nlm.nih.gov/pubmed/>",
+     "chemont: <http://classyfire.wishartlab.com/tax_nodes/>",
     ].each {|uri| print "@prefix #{uri} .\n"}
     print "\n"
   end
@@ -37,25 +38,38 @@ module HMDB
   class Metabolites
 
     def initialize(metabolite_file_path, chemont_file_path)
+      @chemont_label2id = {}
       File.open(metabolite_file_path) do |file|
         @xml = REXML::Document.new(file)
       end
       File.open(chemont_file_path) do |file|
-#        @label2id = parse_chemont(file)  
+        parse_chemont(file)  
       end
       @ontology = Ontology.new
     end
-    attr_accessor :xml, :chemont, :ontology
+    attr_accessor :xml, :chemont, :ontology, :chemont_label2id
 
     def parse_metabolites
       @xml.elements['hmdb'].each_element do |metabolite|
-        m = Metabolite.new(metabolite)
+        m = Metabolite.new(metabolite, @chemont_label2id)
         m.parse_record_information
         m.parse_metabolite_identification
         m.parse_chemical_taxonomy
         m.parse_concentrations(m.get_block('//normal_concentrations'))
         @ontology.parse(m.get_block('//ontology'))
         m.puts_turtle
+      end
+    end
+
+    def parse_chemont(file)
+      while line = file.gets
+        case line
+	when /^id: CHEMONTID:(.+)\n$/ 
+ 	  chemont_id = "C#{$1}"
+ 	when /^name: (.+)\n$/ 
+ 	  @chemont_label2id[$1] = chemont_id
+        else 
+        end
       end
     end
 
@@ -67,15 +81,15 @@ module HMDB
 
   class Metabolite
 
-    def initialize(metabolite)
+    def initialize(metabolite, chemont_label2id)
       @xml = metabolite
+      @chemont_label2id = chemont_label2id
       @accession = @xml.elements['accession'].text
       @triples = []
     end
-    attr_accessor :xml, :accession, :triples
+    attr_accessor :xml, :accession, :triples, :chemont_label2id
 
     def parse_record_information
-#      @accession = @xml.elements['accession'].text
       @triples << [":#{@accession}", "dct:identifier", "\"#{@accession}\""]
       @triples << [":#{@accession}", "ont:version", "\"#{@xml.elements['version'].text}\""]
       @triples << [":#{@accession}", "ont:status", "\"#{@xml.elements['status'].text}\""]
@@ -87,7 +101,6 @@ module HMDB
     end
 
     def parse_metabolite_identification
-#      @accession = @xml.elements['accession'].text
       @triples << [":#{@accession}", "rdfs:label", "\"#{@xml.elements['name'].text}\""]
       @triples << [":#{@accession}", "skos:description", "\"\"\"#{@xml.elements['description'].text}\"\"\"@en"]
       @xml.elements['synonyms'].each_element do |synonym|
@@ -96,11 +109,11 @@ module HMDB
     end
 
     def parse_chemical_taxonomy
-      @accession = @xml.elements['accession'].text
-      @xml.elements['taxonomy'].each_element do |elm|
+#      @accession = @xml.elements['accession'].text
+      @xml.elements['//taxonomy'].each_element do |elm|
         case elm.name
         when 'direct_parent'
-          @triples << [":#{@accession}", "a", "\"#{elm.text}\""]
+          @triples << [":#{@accession}", "a", "chemont:C#{@chemont_label2id[elm.text]}"]
         when 'description'
         when 'alternative_parents'
         when 'substituents'
